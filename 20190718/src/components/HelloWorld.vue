aa7e72946859130b70ee67aafabe9a47fb4c912d
shawn.thecool <shawn.thecool@kakaocorp.com>
2019-07-18 10:49:54 +0900

    init

---
+++ 20190718/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <p>scss variables test</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+p {
+  background-color: $color;
+}
+</style>
---
+++ 20190718/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>reset test</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
